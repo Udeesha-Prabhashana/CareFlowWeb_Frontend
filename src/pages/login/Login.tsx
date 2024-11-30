7644a0481307c47a77ae4108e490232a4f762b61
Gayandee <2021cs153@stu.ucsc.cmb.ac.lk>
2024-11-30 13:30:27 +0530

    User Profile

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { useContext, useState, ChangeEvent, FormEvent } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import "./login.css";
-import jwt_decode from "jwt-decode";
+
 import { toast } from "react-toastify";
 
 interface Credentials {
@@ -17,11 +17,17 @@ const Login = () => {
     password: '',
   });
 
+  const [rememberMe, setRememberMe] = useState(false);
+
   const { loading, error, dispatch } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleRememberMeChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setRememberMe(e.target.checked);
   };
 
   const setCookie = (name: string, value: string, daysToExpire: number) => {
@@ -38,7 +44,7 @@ const Login = () => {
     try {
       console.log("Credentials", credentials);
       console.log(`Basic ${btoa(`${credentials.username}:${credentials.password}`)}`);
-      const res = await axios.post("http://localhost:8080/sign-in", null, {
+      const res = await axios.post(`${process.env.REACT_APP_API_BASE_URL}/sign-in`, null, {
         headers: {
           'Authorization': `Basic ${btoa(`${credentials.username}:${credentials.password}`)}`
         }
@@ -47,9 +53,6 @@ const Login = () => {
       console.log("Response", res);
       console.log("Token", res.data.access_token);
       setCookie('JWT', "Bearer " + res.data.access_token, res.data.access_token_expiry);
-
-      // const decodedToken: JwtPayload = jwt_decode(token);
-      // const userRole = decodedToken.user_role;
 
       dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       toast.success("Login successfully!");
@@ -60,6 +63,8 @@ const Login = () => {
         navigate("/adminhome");
       } else if (res.data.user_role === "ROLE_USER") {
         navigate("/userloginhome");
+      } else if (res.data.user_role === "ROLE_DOCTOR") {
+        navigate("/doctor/home");
       }
     } catch (err: any) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
@@ -84,27 +89,27 @@ const Login = () => {
           className="h-20 mt-8 mb-4 object-contain mr-20"
         />
         <p className="text-center text-gray-700 mr-20">
-          Already have an account?{" "}
-          <span className="text-purple-600 cursor-pointer">Login to your account</span>
+          Don’t have an account ?{" "}
+          <Link to="/register" className="text-violet-600 cursor-pointer">Register in to CareFlow</Link>
         </p>
       </div>
 
       {/* Right Half with Form Fields */}
-      <div className="w-3/5 p-8 flex flex-col justify-center mx-auto ml-24">
+      <div className="w-[550px] p-8 flex flex-col justify-center mx-auto ml-50 mr-20 mt-[0.1px]">
         <h1 className="text-black text-left text-3xl font-semibold mb-6">Login</h1>
-        <div className="mb-4">
+        <div className="mb-8 mt-6">
           <label
             htmlFor="username"
             className="block text-left text-gray-700 text-lg font-roboto font-normal leading-6 mb-2"
           >
-            Username
+            Username/email
           </label>
           <input
             id="username"
             type="text"
             onChange={handleChange}
-            placeholder="Enter your username"
-            className="w-4/5 px-4 py-2 border border-gray-300 rounded-lg shadow-md"
+            placeholder="Enter your username or email"
+            className="w-[300px] h-[30px] px-4 py-2 border border-violet-500 rounded-lg shadow-md"
             value={credentials.username}
           />
         </div>
@@ -120,18 +125,37 @@ const Login = () => {
             type="password"
             onChange={handleChange}
             placeholder="Enter your password"
-            className="w-4/5 px-4 py-2 border border-gray-300 rounded-lg shadow-md"
+            className="w-[300px] h-[30px] px-4 py-2 border border-violet-500 rounded-lg shadow-md"
             value={credentials.password}
           />
+        </div>
+        <div className="mb-1 mt-3">
+          <input
+            id="rememberMe"
+            type="checkbox"
+            onChange={handleRememberMeChange}
+            checked={rememberMe}
+            className="mr-2"
+          />
+          <label
+            htmlFor="rememberMe"
+            className="text-left text-gray-500 text-lg font-roboto font-normal leading-6"
+          >
+            Remember Me
+          </label>
         </div>
         <button
           disabled={loading}
           onClick={handleClick}
-          className="bg-purple-600 text-white px-4 py-2 mt-8 rounded-lg w-4/5"
+          className="bg-violet-600 text-white px-4 py-2 mt-8 rounded-lg w-[335px]"
         >
           Login
         </button>
         {error && <span className="text-red-500 mt-8">{error.message}</span>}
+        <div className="mt-4">
+          <span className="text-gray-600 cursor-pointer">Forgot password? </span>
+          <span className="text-violet-600 cursor-pointer">Click here</span>
+        </div>
       </div>
     </div>
   );
